8771d80fd52910ed5cdfd1d738cefa2b06178db6
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-04 01:03:54 +0300

    Initial commit

---
+++ src/main/java/com/gucarsoft/account/AccountApplication.java
@@ -0,0 +1,13 @@
+package com.gucarsoft.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
