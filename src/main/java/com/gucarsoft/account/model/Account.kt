5a568b79ff953cfacdfbaaa4293ba6363349fcc1
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-05 01:00:46 +0300

    models updated

--- src/main/java/com/gucarsoft/account/model/Account.kt
+++ src/main/java/com/gucarsoft/account/model/Account.kt
@@ -18,11 +18,18 @@ data class Account(
         @JoinColumn(name = "customer_id", nullable = false)
         val customer: Customer?,
 
-        @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
-        val transaction: Set<Transaction>?,
+        @OneToMany(mappedBy = "account", fetch = FetchType.EAGER,cascade = [CascadeType.ALL])
+        val transaction: Set<Transaction> = HashSet(),
 
         )
 {
+        constructor(customer:Customer,balance:BigDecimal,creationDate: LocalDateTime): this(
+                "",
+                customer=customer,
+                balance=balance,
+                creationDate=creationDate,
+        )
+
         override fun equals(other: Any?): Boolean {
                 if (this === other) return true
                 if (javaClass != other?.javaClass) return false
