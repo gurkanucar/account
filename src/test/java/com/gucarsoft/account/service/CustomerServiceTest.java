9612e23b35ff58f0a6aee71c97d1109e05e8f51f
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-11 12:00:36 +0300

    test methods updated

--- src/test/java/com/gucarsoft/account/service/CustomerServiceTest.java
+++ src/test/java/com/gucarsoft/account/service/CustomerServiceTest.java
@@ -1,6 +1,8 @@
 package com.gucarsoft.account.service;
 
+import com.gucarsoft.account.dto.CustomerDTO;
 import com.gucarsoft.account.dto.CustomerDTOConverter;
+import com.gucarsoft.account.exception.CustomerNotFoundException;
 import com.gucarsoft.account.model.Customer;
 import com.gucarsoft.account.repository.CustomerRepository;
 
@@ -39,7 +41,27 @@ public class CustomerServiceTest {
                 customer);
     }
 
+    @Test
+    public void testFindByCustomerId_whenCustomerIdDoesNotExists_shouldThrowCustomerNotFoundException(){
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.empty());
+       Assertions.assertThrows(CustomerNotFoundException.class,()->service.findCustomerById("id"));
+    }
 
+    @Test
+    public void testGetCustomerById_whenCustomerIdExists_shouldReturnCustomer(){
+        Customer customer = new Customer("id", "name", "surname", Set.of());
+        CustomerDTO customerDTO=new CustomerDTO("id","name","surname",Set.of());
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.of(customer));
+        Mockito.when(converter.convertToCustomerDTO(customer)).thenReturn(customerDTO);
+        CustomerDTO result = service.getCustomerById("id");
+        Assertions.assertEquals(result,
+                customerDTO);
+    }
 
-
+    @Test
+    public void testGetCustomerById_whenCustomerIdDoesNotExists_shouldThrowCustomerNotFoundException() {
+        Mockito.when(customerRepository.findById("id")).thenReturn(Optional.empty());
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> service.getCustomerById("id"));
+        Mockito.verifyNoInteractions(converter);
+    }
 }
